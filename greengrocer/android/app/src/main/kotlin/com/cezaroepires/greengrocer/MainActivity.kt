df0bd1baa8f8846aa2cbbc6d96b823162219e7ec
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-07-18 22:40:39 -0300

    Start of development

---
+++ greengrocer/android/app/src/main/kotlin/com/cezaroepires/greengrocer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cezaroepires.greengrocer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ greengrocer/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/auth/sign_in_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GreenGrocer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SignInScreen(),
+    );
+  }
+}
