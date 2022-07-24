53e58f29a2f74a32ad194ae9751887942070797c
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-07-24 00:33:31 -0300

    Adding cart button and price format

--- greengrocer/lib/src/pages/home/components/item_tile.dart
+++ greengrocer/lib/src/pages/home/components/item_tile.dart
@@ -1,61 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/services/utils_services.dart';
 
 class ItemTile extends StatelessWidget {
-  const ItemTile({Key? key, required this.item}) : super(key: key);
+  ItemTile({Key? key, required this.item}) : super(key: key);
 
   final ItemModel item;
 
+  final UtilsServices utilsServices = UtilsServices();
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      shadowColor: Colors.grey.shade300,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20.0),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Imagem
-            Expanded(
-              child: Image.asset(item.imgUrl),
+    return Stack(
+      children: [
+        Card(
+          elevation: 2,
+          shadowColor: Colors.grey.shade300,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // Imagem
+                Expanded(
+                  child: Image.asset(item.imgUrl),
+                ),
+                // Nome Produto
+                Text(
+                  item.itemName,
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                // Preço - Unidade - Produto
+                Row(
+                  children: [
+                    Text(
+                      utilsServices.priceToCurrency(item.price),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: CustomColors.customSwatchColor,
+                      ),
+                    ),
+                    Text(
+                      "/${item.unit}",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12.0,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                  ],
+                )
+              ],
             ),
-            // Nome Produto
-            Text(
-              item.itemName,
-              style: const TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold,
+          ),
+        ),
+        // Botão adicionar carrinho
+        Positioned(
+          top: 4,
+          right: 4,
+          child: GestureDetector(
+            child: Container(
+              height: 40,
+              width: 35,
+              decoration: BoxDecoration(
+                color: CustomColors.customSwatchColor,
+                borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(15.0),
+                    topRight: Radius.circular(20.0)),
+              ),
+              child: const Icon(
+                Icons.add_shopping_cart_outlined,
+                color: Colors.white,
+                size: 20,
               ),
             ),
-            // Preço - Unidade - Produto
-            Row(
-              children: [
-                Text(
-                  item.price.toString(),
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,
-                    color: CustomColors.customSwatchColor,
-                  ),
-                ),
-                Text(
-                  "/${item.unit}",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 12.0,
-                    color: Colors.grey.shade500,
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+          ),
+        )
+      ],
     );
   }
 }
