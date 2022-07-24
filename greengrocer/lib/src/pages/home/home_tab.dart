d4bb743d3e2f77e1a9128f2407594eadcd6cdbc3
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-07-24 00:00:25 -0300

    Organization of the project and creation of mocks

--- greengrocer/lib/src/pages/home/home_tab.dart
+++ greengrocer/lib/src/pages/home/home_tab.dart
@@ -1,7 +1,7 @@
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
-import 'package:greengrocer/src/home/components/category_tile.dart';
+import 'package:greengrocer/src/pages/home/components/category_tile.dart';
 
 class HomeTab extends StatefulWidget {
   const HomeTab({Key? key}) : super(key: key);
@@ -11,13 +11,7 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
-  List<String> categories = [
-    "Frutas",
-    "Grãos",
-    "Verduras",
-    "Temperos",
-    "Cereais",
-  ];
+
 
   String selectedCategory = 'Frutas';
 
