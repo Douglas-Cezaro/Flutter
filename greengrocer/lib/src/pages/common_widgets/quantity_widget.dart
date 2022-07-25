7ae7208157ddea324f8995fea8f125f2b603bf1e
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-07-25 15:47:30 -0300

    Increasing and decreasing the amount

--- greengrocer/lib/src/pages/common_widgets/quantity_widget.dart
+++ greengrocer/lib/src/pages/common_widgets/quantity_widget.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 
 class QuantityWidget extends StatelessWidget {
-  const QuantityWidget({Key? key}) : super(key: key);
+  final int value;
+  final String suffixText;
+  final Function(int quantity) result;
+
+  const QuantityWidget({
+    Key? key,
+    required this.suffixText,
+    required this.value,
+    required this.result,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,13 +29,17 @@ class QuantityWidget extends StatelessWidget {
           _QuantityButton(
             icon: Icons.remove,
             color: Colors.grey,
-            onPressed: () {},
+            onPressed: () {
+              if(value == 1) return;
+              int resultCount = value - 1;
+              result(resultCount);
+            },
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 6.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 6.0),
             child: Text(
-              "1Kg",
-              style: TextStyle(
+              "$value$suffixText",
+              style: const TextStyle(
                 fontSize: 15.0,
                 fontWeight: FontWeight.bold,
               ),
@@ -35,7 +48,10 @@ class QuantityWidget extends StatelessWidget {
           _QuantityButton(
             icon: Icons.add,
             color: CustomColors.customSwatchColor,
-            onPressed: () {},
+            onPressed: () {
+              int resultCount = value + 1;
+              result(resultCount);
+            },
           ),
         ],
       ),
