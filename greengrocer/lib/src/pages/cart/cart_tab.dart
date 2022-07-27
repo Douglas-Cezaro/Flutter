135303ec0df40a26de1f3d85382a3cd9ad1515c0
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-07-26 21:55:34 -0300

    confirmation dialog

--- greengrocer/lib/src/pages/cart/cart_tab.dart
+++ greengrocer/lib/src/pages/cart/cart_tab.dart
@@ -82,7 +82,10 @@ class _CartTabState extends State<CartTab> {
                         borderRadius: BorderRadius.circular(18.0),
                       ),
                     ),
-                    onPressed: () {},
+                    onPressed: () async {
+                      bool? result = await showOrderConfirmation();
+                      print(result);
+                    },
                     child: const Text(
                       "Concluir pedido",
                       style: TextStyle(fontSize: 18.0),
@@ -95,5 +98,34 @@ class _CartTabState extends State<CartTab> {
         ],
       ),
     );
+  }
+
+  Future<bool?> showOrderConfirmation() {
+    return showDialog<bool>(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            title: const Text("Confirmação"),
+            content: const Text("Deseja realmente concluir o pedido"),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: const Text("Não"),
+              ),
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: const Text("Sim"),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+              )
+            ],
+          );
+        });
   }
 }
