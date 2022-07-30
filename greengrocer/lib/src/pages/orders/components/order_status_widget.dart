c2d4679d0e2f169ce8765cd3e8fb88d491036a8b
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-07-30 01:21:36 -0300

    Starting Status Widget Development

---
+++ greengrocer/lib/src/pages/orders/components/order_status_widget.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+
+class OrderStatusWidget extends StatelessWidget {
+  final String status;
+  final bool isOverdue;
+
+  const OrderStatusWidget({
+    Key? key,
+    required this.status,
+    required this.isOverdue,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _StatusDot(
+          isActive: true,
+          title: 'Pedido confirmado',
+        ),
+        _StatusDot(
+          isActive: false,
+          title: 'Teste',
+        )
+      ],
+    );
+  }
+}
+
+class _StatusDot extends StatelessWidget {
+  final bool isActive;
+  final String title;
+
+  const _StatusDot({
+    Key? key,
+    required this.isActive,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        // Dot
+        Container(
+          alignment: Alignment.center,
+          height: 20,
+          width: 20,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: CustomColors.customSwatchColor,
+            ),
+            color:
+                isActive ? CustomColors.customSwatchColor : Colors.transparent,
+          ),
+          child: isActive
+              ? const Icon(
+                  Icons.check,
+                  size: 13.0,
+                  color: Colors.white,
+                )
+              : const SizedBox.shrink(),
+        ),
+        // Texto
+        Expanded(child: Text(title)),
+      ],
+    );
+  }
+}
