1e65df644b1112467c5ce6fe900abcf39942c3e2
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-07-30 01:04:51 -0300

    Product listing

--- greengrocer/lib/src/pages/orders/components/order_tile.dart
+++ greengrocer/lib/src/pages/orders/components/order_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:greengrocer/src/models/cart_item_model.dart';
 import 'package:greengrocer/src/models/order_model.dart';
 import 'package:greengrocer/src/services/utils_services.dart';
 
@@ -22,6 +23,7 @@ class OrderTile extends StatelessWidget {
           dividerColor: Colors.transparent,
         ),
         child: ExpansionTile(
+          childrenPadding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
           title: Column(
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -36,8 +38,68 @@ class OrderTile extends StatelessWidget {
               ),
             ],
           ),
-          children: [],
+          children: [
+            SizedBox(
+              height: 150,
+              child: Row(
+                children: [
+                  Expanded(
+                      flex: 3,
+                      child: ListView(
+                        children: order.items.map((orderItem) {
+                          return _OrderItemWidget(
+                            orderItem: orderItem,
+                            utilsServices: _utilsServices,
+                          );
+                        }).toList(),
+                      )),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.blue,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class _OrderItemWidget extends StatelessWidget {
+  const _OrderItemWidget({
+    Key? key,
+    required this.utilsServices,
+    required this.orderItem,
+  }) : super(key: key);
+
+  final UtilsServices utilsServices;
+  final CartItemModel orderItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: Row(
+        children: [
+          Text(
+            '${orderItem.quantity} ${orderItem.item.unit} ',
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Expanded(
+            child: Text(orderItem.item.itemName),
+          ),
+          Text(
+            utilsServices.priceToCurrency(
+              orderItem.totalPrice(),
+            ),
+          )
+        ],
       ),
     );
   }
