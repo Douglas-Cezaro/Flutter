6a0980b68e8d2e52a1333efeb1a58986ccff5f95
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-07-31 17:59:08 -0300

    Creating the splash screen

--- greengrocer/lib/src/pages/auth/sign_in_screen.dart
+++ greengrocer/lib/src/pages/auth/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
+import 'package:greengrocer/src/pages/common_widgets/app_name_widget.dart';
 import 'package:greengrocer/src/pages/common_widgets/custom_text_field.dart';
 import 'package:greengrocer/src/pages/auth/sign_up_screen.dart';
 
@@ -27,26 +28,9 @@ class SignInScreen extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     // Nome do app
-                    Text.rich(
-                      TextSpan(
-                          style: const TextStyle(
-                            fontSize: 40.0,
-                          ),
-                          children: [
-                            const TextSpan(
-                              text: "Green",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            TextSpan(
-                              text: "grocer",
-                              style: TextStyle(
-                                color: CustomColors.customContrastColor,
-                              ),
-                            ),
-                          ]),
+                    const AppNameWidget(
+                      greenTitleColor: Colors.white,
+                      textSize: 40.0,
                     ),
                     // Categorias
                     SizedBox(
