bd46f497317e8eed21ace4878110fe2d0ff25793
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-07-31 17:29:02 -0300

    Button tap animation

--- greengrocer/lib/src/pages/home/components/category_tile.dart
+++ greengrocer/lib/src/pages/home/components/category_tile.dart
@@ -15,7 +15,8 @@ class CategoryTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
+    return InkWell(
+      borderRadius: BorderRadius.circular(10.0),
       onTap: onPressed,
       child: Align(
         alignment: Alignment.center,
