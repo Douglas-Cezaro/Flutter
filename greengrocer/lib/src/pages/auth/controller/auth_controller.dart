b9d84e580903af0de85dfbc5090ca170009f86f9
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-10-09 21:27:59 -0300

    Navigating to base

--- greengrocer/lib/src/pages/auth/controller/auth_controller.dart
+++ greengrocer/lib/src/pages/auth/controller/auth_controller.dart
@@ -1,11 +1,17 @@
 import 'package:get/get.dart';
+import 'package:greengrocer/src/models/user_model.dart';
 import 'package:greengrocer/src/pages/auth/repository/auth_repository.dart';
 import 'package:greengrocer/src/pages/auth/result/auth_result.dart';
+import 'package:greengrocer/src/pages_routes/app_pages.dart';
+import 'package:greengrocer/src/services/utils_services.dart';
 
 class AuthController extends GetxController {
   RxBool isLoading = false.obs;
 
   final AuthRepository authRepository = AuthRepository();
+  final utilsServices = UtilsServices();
+
+  UserModel user = UserModel();
 
   Future<void> singIn({
     required String email,
@@ -19,9 +25,10 @@ class AuthController extends GetxController {
     isLoading.value = false;
 
     result.when(success: (user) {
-      print(user);
+      this.user = user;
+      Get.offAllNamed(PageRoutes.baseRoute);
     }, error: (message) {
-      print(message);
+      utilsServices.showToast(message: message, isError: true);
     });
   }
 }
