f23ac8bc5f3bedcc49479b3f54b4c1fbf6759d72
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-10-10 15:43:37 -0300

    Created Widgets

--- dartweek/fwc_album_app/lib/app/pages/splash/splash_page.dart
+++ dartweek/fwc_album_app/lib/app/pages/splash/splash_page.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:fwc_album_app/app/core/styles/button_styles.dart';
-import 'package:fwc_album_app/app/core/styles/text_styles.dart';
-import 'package:fwc_album_app/app/core/styles/colors_app.dart';
+import 'package:fwc_album_app/app/core/ui/helpers/loader.dart';
+import 'package:fwc_album_app/app/core/ui/styles/button_styles.dart';
+import 'package:fwc_album_app/app/core/ui/styles/text_styles.dart';
+import 'package:fwc_album_app/app/core/ui/widgets/button.dart';
+import 'package:fwc_album_app/app/core/ui/widgets/rounded_button.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> with Loader {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
       appBar: AppBar(
         title: const Text('Splash Page'),
       ),
@@ -17,7 +23,13 @@ class SplashPage extends StatelessWidget {
           child: Column(
         children: [
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () async {
+              showLoader();
+
+              await Future.delayed(Duration(seconds: 2));
+
+              hideLoader();
+            },
             style: ButtonStyles.i.yellowButton,
             child: Text("Salvar"),
           ),
@@ -36,7 +48,14 @@ class SplashPage extends StatelessWidget {
             style: ButtonStyles.i.primaryOutlineButton,
             child: Text("Salvar"),
           ),
-          TextField()
+          TextField(),
+          Button(
+            style: ButtonStyles.i.primaryButton,
+            labelStyle: context.textStyles.textPrimaryFontBold,
+            label: "Salvar",
+            onPressed: (() {}),
+          ),
+          RoundedButton(icon: Icons.add_sharp, onPressed: () {})
         ],
       )),
     );
