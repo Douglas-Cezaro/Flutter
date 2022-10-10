46c0a5613e75b04e7eedabde9f633fbe4ff6e3b1
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-10-09 21:19:36 -0300

    Data matching with Freezed

--- greengrocer/lib/src/pages/auth/repository/auth_repository.dart
+++ greengrocer/lib/src/pages/auth/repository/auth_repository.dart
@@ -1,11 +1,15 @@
 import 'package:greengrocer/src/constants/endpoints.dart';
 import 'package:greengrocer/src/models/user_model.dart';
+import 'package:greengrocer/src/pages/auth/repository/auth_errors.dart'
+    as auth_errors;
+import 'package:greengrocer/src/pages/auth/result/auth_result.dart';
 import 'package:greengrocer/src/services/http_manager.dart';
 
 class AuthRepository {
   final HttpManager _httpManager = HttpManager();
 
-  Future signIn({required String email, required String password}) async {
+  Future<AuthResult> signIn(
+      {required String email, required String password}) async {
     final result = await _httpManager.restRequest(
       url: Endpoints.signin,
       method: HttpMethods.post,
@@ -16,13 +20,10 @@ class AuthRepository {
     );
 
     if (result["result"] != null) {
-      print("FUNCIONOU");
-      print(result["result"]);
       final user = UserModel.fromJson(result["result"]);
-      print(user);
+      return AuthResult.success(user);
     } else {
-      print("Nao funcionou");
-      print(result["error"]);
+      return AuthResult.error(auth_errors.authErrorsString(result["error"]));
     }
   }
 }
